9073bb74bbf430d6fe2f08fd3e61146e03b78afe
Nnenna-udefi <miriamudefi@gmail.com>
2024-11-10 17:08:57 +0100

    made the contact page responsive

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -7,16 +7,18 @@ import Link from "next/link";
 
 export const Contact = () => {
   return (
-    <div className="bg-pnk text-white px-14 pt-32 h-screen">
+    <div className="bg-pnk text-white px-6 md:px-14 pt-32 h-screen">
       <motion.div
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5, delay: 0.1 }}
       >
-        <h1 className="text-white text-5xl">Contact Us</h1>
-        <p className="text-4xl my-6">We&apos;d love to hear from you</p>
+        <h1 className="text-white text-4xl md:text-5xl">Contact Us</h1>
+        <p className="text-3xl md:text-4xl my-6">
+          We&apos;d love to hear from you
+        </p>
       </motion.div>
-      <p className="text-2xl">
+      <p className="text-xl md:text-2xl">
         Whether you’re new to HerRise or have been with us for years, we’re glad
         you’re here.
       </p>
