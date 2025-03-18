65a157461b3b8f8411236f5b298fe35ceabc9901
Nnenna-udefi <miriamudefi@gmail.com>
2025-03-18 09:57:35 +0100

    updated hero image

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,14 +1,17 @@
 import React from "react";
+import heroImg from "@/assets/images/klingAI.png";
+import Image from "next/image";
 
 export const Hero = () => {
   return (
     <div className=" pt-20 w-full ">
-      <div className=" bg-hero2 bg-right mt-5  bg-cover bg-no-repeat w-full h-[700px]">
-        <div className=" flex flex-col gap-4 pt-96 md:px-12 px-4">
-          <p className="md:text-5xl bg-pnk text-white p-4 text-4xl w-full md:w-2/3">
+      <div className="  bg-right mt-5  bg-cover bg-no-repeat w-full h-[700px]">
+        <div className=" flex flex-col md:flex-row gap-4 pt-20 md:px-12 px-4">
+          <p className="md:text-5xl bg-pnk text-white p-4 flex items-center text-4xl w-full md:w-2/3">
             Empowering Women, Educating Communities, Advocating for a
             Sustainable Future
           </p>
+          <Image src={heroImg} alt="hero" width={500} height={500} />
         </div>
       </div>
     </div>
