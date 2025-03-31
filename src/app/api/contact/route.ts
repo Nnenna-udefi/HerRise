fbcfdfccb2eb181a4d6b856f75fd6db47c47bfe5
Nnenna Udefi <miriamudefi@gmail.com>
2025-03-31 19:07:08 +0100

    Update route.ts

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -18,7 +18,7 @@ export async function POST(req: Request) {
       { message: "Message sent successfully" },
       { status: 200 }
     );
-  } catch (error) {
+  } catch (error: unknown) {
     return NextResponse.json(
       { error: "Failed to send message" },
       { status: 500 }
